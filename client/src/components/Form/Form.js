9e77cf1ee49d9e3e1f27a50d18138c5eb927065a
Wanming123 <hwm020926@icloud.com>
2025-02-15 18:30:00 -0500

    Create a Post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import { Root, StyledPaper, StyledForm, FileInput, ButtonSubmit } from './styles';
+import React, { useState } from 'react';
+import { TextField, Button, Typography } from '@mui/material';
+import { Root, StyledPaper, FileInput, ButtonSubmit } from './styles';
+import { useDispatch } from 'react-redux';
+
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
+  const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const dispatch = useDispatch();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData))
+  }
+  
+  const clear = () => {
+  }
+  
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPostData({ ...postData, selectedFile: reader.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
   return (
-    <h1>FORM</h1>
+    <StyledPaper>
+      <Root autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+        <FileInput>
+          <input type="file" accept="image/*" onChange={handleFileChange} />
+        </FileInput>
+        <ButtonSubmit variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</ButtonSubmit>
+        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+      </Root>
+    </StyledPaper>
   )
 }
 
