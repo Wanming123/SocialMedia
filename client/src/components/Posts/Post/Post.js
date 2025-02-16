2127865147f5ae99f7cf19225fa18bef419a86f4
Wanming123 <hwm020926@icloud.com>
2025-02-16 18:30:00 -0500

    post layout

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import { StyledCard, Media, Overlay, Overlay2, Details, Title, StyledCardActions, CardAction } from './styles';
+import { CardContent, Button, Typography } from '@mui/material';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import moment from 'moment';
+import { StyledCard, Media, Overlay, Overlay2, Details, Title, StyledCardActions } from './styles';
 
-
-const POST = () => {
+const POST = ({ post }) => {
   return (
-    <h1>Posts</h1>
+    <StyledCard>
+      <Media image={post.selectedFile} title={post.title} />
+      <Overlay>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </Overlay>
+      <Overlay2>
+        <Button style={{ color: 'white' }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </Overlay2>
+      <Details>
+        <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+      </Details>
+      <CardContent>
+        <Title>
+          <Typography variant="h5" gutterBottom>{post.message}</Typography>
+        </Title>
+      </CardContent>
+      <StyledCardActions>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </StyledCardActions>
+    </StyledCard>
   )
 }
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
-import { MainContainer, Heading, Image, Toolbar, Profile, UserName, BrandContainer, SmMargin, Purple, ActionDiv } from './styles';
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
 
   return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6} lg={3}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   )
 }
 
